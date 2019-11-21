a619d261e052b35d24538d2dfae9d0dbb74ecf11
戈乾志 <geqianzhi@sinoecare.com>
2019-11-21 21:43:55 +0800

    first

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+
+main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("my app"),
+        ),
+        body: HomeContent(),
+      ),
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Checkbox(
+              value: true,
+              onChanged: (value) => print("Hello World")),
+          Text(
+            "同意协议",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(fontSize: 20),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
